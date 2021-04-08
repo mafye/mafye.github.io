416f408693abde44435d476c0be29c3fdd7c7dfb
Machao <34216039+mafye@users.noreply.github.com>
2021-04-08 19:36:57 +0800

    add files
    first commit

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,16 @@
+"use strict";
+
+
+
+{
+	const scriptsInEvents = {
+
+		async 事件表1_Event1_Act7(runtime, localVars)
+		{
+			alert("哈哈 一个小游戏")
+		}
+
+	};
+	
+	self.C3.ScriptsInEvents = scriptsInEvents;
+}
